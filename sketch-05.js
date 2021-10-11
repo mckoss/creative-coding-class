1dd681573dfc3a1737bc006e4e4ed99364e2eb63
mckoss <>
2021-10-11 04:05:51 +0000

    Add TweakPane and factor getGlyphDots

--- sketch-05.js
+++ sketch-05.js
@@ -1,7 +1,14 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+import {Pane} from 'tweakpane';
+
+import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
+const interpolate = require('color-interpolate');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 let manager;
@@ -14,18 +21,61 @@ let glyph = 'K';
 const glyphCanvas = document.createElement('canvas');
 const glyphContext = glyphCanvas.getContext('2d');
 
-const sketch = ({width, height}) => {
-  const cell = 20;
-  const cols = width / cell;
-  const rows = height / cell;
-  glyphCanvas.width = cols;
-  glyphCanvas.height = rows;
+// Tweakpane parameters
+const params = {
+  speed: 4,
+  cell: 20,
+  filter: { min: 0, max: 180 },
+  radius: 0.45,
+}
 
+function createPane() {
+  const pane = new Pane();
+  pane.registerPlugin(EssentialsPlugin);
+
+  const fParams = pane.addFolder({title: 'Params'});
+  fParams.addInput(params, 'speed', {min: 1, max: 20});
+  fParams.addInput(params, 'cell', { min: 5, max: 100, step: 1 });
+  fParams.addInput(params, 'filter', { min: 0, max: 255, step: 1 });
+  fParams.addInput(params, 'radius', { min: 0.05, max: 0.6 });
+}
+
+const sketch = ({width, height}) => {
   return ({ context, width, height }) => {
-    glyphContext.fillStyle = 'black';
-    glyphContext.fillRect(0, 0, cols, rows);
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    const cell = params.cell;
+    const cols = Math.floor(width / cell);
+    const rows = Math.floor(height / cell);
+
+    let dots = getGlyphDots(glyph, rows, cols);
+
+    for (let dot of dots) {
+        const [x, y, v] = dot;
+        context.fillStyle = `rgb(${v}, ${v}, ${v})`;
+
+        context.save();
+        context.translate(x * cell, y * cell);
+        context.beginPath()
+        context.arc(cell/2, cell/2, cell * params.radius, 0, 2 * Math.PI);
+        context.fill();
+        context.restore();
+    }
+  };
+};
+
+function getGlyphDots(glyph, rows, cols) {
+    // array of [x, y, value: 0 - 255]
+    let results = [];
+
+    glyphCanvas.width = cols;
+    glyphCanvas.height = rows;
 
     glyphContext.fillStyle = 'white';
+    glyphContext.fillRect(0, 0, cols, rows);
+
+    glyphContext.fillStyle = 'black';
     glyphContext.font = `${cols}px ${fontFamily}`;
 
     const metrics = glyphContext.measureText(glyph);
@@ -49,25 +99,18 @@ const sketch = ({width, height}) => {
         let col = i % cols;
         let row = Math.floor(i / cols);
 
-        const r = glyphPixels[4 * i];
+        // const r = glyphPixels[4 * i];
         const g = glyphPixels[4 * i + 1];
-        const b = glyphPixels[4 * i + 2];
-        const a = glyphPixels[4 * i + 3];
+        // const b = glyphPixels[4 * i + 2];
+        // const a = glyphPixels[4 * i + 3];
 
-        context.fillStyle = `rgb(${r}, ${g}, ${b})`;
-
-        context.save();
-        context.translate(col * cell, row * cell);
-        context.beginPath()
-        context.arc(cell/2, cell/2, cell * 0.4, 0, 2 * Math.PI);
-        context.fill();
-        context.restore();
+        if (g >= params.filter.min || g <= params.filter.max) {
+            results.push([col, row, g]);
+        }
     }
 
-    // context.drawImage(glyphCanvas, 0, 0);
-
-  };
-};
+    return results;
+}
 
 async function start() {
     manager = await canvasSketch(sketch, settings);
@@ -81,4 +124,5 @@ document.addEventListener('keyup', (e) => {
     }
 });
 
+createPane();
 start();
